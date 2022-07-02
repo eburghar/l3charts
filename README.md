4892e7a29991e9298adeeb36ec4c731b8b47421d
Éric BURGHARD <eric.burghard@itsufficient.me>
2022-07-02 09:37:18 +0200

    chore: add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# l3charts
+
+This package defines a few simple TikZ charts that can be drawn using LaTeX
+environments. This has mainly been developped as an experimentation of `expl3`
+for checking what LaTeX3 really brought to facilitate package developpement (expansion
+control, clist, seq, prop, ...).
---
+++ build.lua
@@ -0,0 +1,33 @@
+-- build script for l3charts.dtx
+module = "l3charts"
+bundle = ""
+
+-- Typeset configuration
+typesetexe = "lualatex"
+typesetopts = "--interaction=nonstopmode --shell-escape --recorder"
+typesetdemofiles = {
+    "l3charts-doc.tex"
+}
+
+-- Copy all files match pattern to build/doc
+-- therefore use \input{java-program} instead of \input{demo-code/java-program.tex}
+sourcefiles = {
+    "l3charts.sty"
+}
+
+installfiles = sourcefiles
+
+uploadconfig = {
+    pkg          = "l3charts",
+    version      = "0.1.0",
+    author       = "Éric BURGHARD",
+    uploader     = "Éric BURGHARD",
+    email        = "ctan@itsufficient.me",
+    license      = "lppl1.3c",
+    summary      = "Customizable charts made with latex3",
+    ctanPath     = "/graphics/pgf/contrib/l3charts",
+    home         = "https://github.com/eburghar/l3charts.git",
+    repository   = "https://github.com/eburghar/l3charts.git",
+    bugtracker   = "https://github.com/eburghar/l3charts/issues",
+    note         = [[Uploaded automatically by l3build]]
+  }
