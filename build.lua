251df4cad9502fefd3e8fcde86f3f80187034aac
Éric BURGHARD <eric.burghard@itsufficient.me>
2022-07-06 11:26:54 +0200

    chore(version): 0.2.0

--- build.lua
+++ build.lua
@@ -12,14 +12,16 @@ typesetdemofiles = {
 -- Copy all files match pattern to build/doc
 -- therefore use \input{java-program} instead of \input{demo-code/java-program.tex}
 sourcefiles = {
-    "l3charts.sty"
+    "*.sty", "*.cls", "*.ist"
 }
+
+indexstyle = "l3charts-doc.ist"
 
 installfiles = sourcefiles
 
 uploadconfig = {
     pkg          = "l3charts",
-    version      = "0.1.0",
+    version      = "0.2.0",
     author       = "Éric BURGHARD",
     uploader     = "Éric BURGHARD",
     email        = "ctan@itsufficient.me",
